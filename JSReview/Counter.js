7929bbcd3fa61589956ce7704c8dfd9afb1c6dbf
Milkacsoki140 <kissmilan40@gmailcom>
2019-10-01 09:19:46 +0200

    web prog 4 counter, calculator és index JS cnt duplikalva most már rendesen

--- JSReview/Counter.js
+++ JSReview/Counter.js
@@ -1,10 +1,28 @@
+class Counter{
+    construction(cnt_init){
+        if(cnt_init === null)
+        {
+            this.cnt = 0;
+        }
+        else{
+            this.cnt = cnt_init;
+        }
+
+    }
+    increase(){
+        this.cnt++;
+    }
+    decrease(){
+        this.cnt--;
+    }
+    counter(){
+        return this.cnt;
+    }
+}
+
 var cnt = 0;
 var incrise = () => {counter++};
 var decrease = () => {counter--};
 var counter = () => {return cnt};
 
-module.exports = {
-    inc : incrise,
-    dec : decrease,
-    val : counter
-};
+module.exports = Counter;
