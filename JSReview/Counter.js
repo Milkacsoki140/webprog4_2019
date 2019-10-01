8ea5cbcc60641bc201329ead3a64c2bbb6543744
Milkacsoki140 <kissmilan40@gmailcom>
2019-10-01 09:21:15 +0200

    web prog 4 counter, calculator és index JS cnt dulikalas pipa

--- JSReview/Counter.js
+++ JSReview/Counter.js
@@ -1,28 +1,21 @@
 class Counter{
-    construction(cnt_init){
-        if(cnt_init === null)
-        {
+    constructor(cnt_init){
+        if(cnt_init === null){
             this.cnt = 0;
         }
-        else{
+        else {
             this.cnt = cnt_init;
         }
-
     }
+
     increase(){
         this.cnt++;
     }
     decrease(){
         this.cnt--;
     }
-    counter(){
+    value(){
         return this.cnt;
     }
 }
-
-var cnt = 0;
-var incrise = () => {counter++};
-var decrease = () => {counter--};
-var counter = () => {return cnt};
-
 module.exports = Counter;
